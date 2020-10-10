63a5bf47801b6909627c5d7b7df54cf005c0d14e
sallyvellini <67206023+sallyvellini@users.noreply.github.com>
2020-10-10 20:44:26 +0400

    Update create.php

--- create.php
+++ create.php
@@ -26,7 +26,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $input_name = trim($_POST["name"]);
   if (empty($input_name)) {
     $name_err = "Please enter a name.";
-  } elseif (!filter_var($input_name, FILTER_VALIDATE_REGEXP, array("options" => array("regexp" => "/^[a-zA-Z\s]+$/")))) {
+  } elseif (!filter_var($input_name, FILTER_VALIDATE_REGEXP, array("options" => array("regexp" => "/^[a-zA-Z1-9\s]+$/")))) {
     $name_err = "Please enter a valid name.";
   } else {
     $name = $input_name;
